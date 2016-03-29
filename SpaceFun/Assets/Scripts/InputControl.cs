e6ec1a03301ce635088af1dda4e0da4ecc9d2051
Peter Rossing <hprossing@gmail.com>
2016-03-29 21:38:54 +0200

    Input toggle added
    In-game, press "t" to switch between keyboard and xbox controller.
    On keyboard, i+j+k+l = right analog.
    Keyboard is default.
    Also added placeholder models.

--- SpaceFun/Assets/Scripts/InputControl.cs
+++ SpaceFun/Assets/Scripts/InputControl.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class InputControl : MonoBehaviour {
 
 	public float LV, LH, RV, RH, LB, RB;
+	public int inputDevice = 0;
+	float delay = 0.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -18,31 +20,64 @@ public class InputControl : MonoBehaviour {
 		Debug.Log ("RH: " + Input.GetAxis ("PS4_RightAnalogHorizontal"));
 		Debug.Log ("RV: " + Input.GetAxis ("PS4_RightAnalogVertical"));
 		*/
+
+		//Toggle input method
+		// 0=Keyboard
+		// 1=Xbox controller
+		if(Input.GetKey("t") && Time.time > delay){
+			delay = Time.time + 0.5f;
+			inputDevice++;
+			if(inputDevice==2){
+				inputDevice=0;
+			}
+			switch(inputDevice){
+			case 0:
+				Debug.Log ("Keyboard is now active input device");
+				break;
+			case 1:
+				Debug.Log ("Xbox Controller is now active input device");
+				break;
+			}
+
+
+		}
+
 		LH = Input.GetAxis ("Horizontal");
 		LV = Input.GetAxis ("Vertical");
-		RH = Input.GetAxis ("Xbox_RAnalogH");
-		RV = Input.GetAxis ("Xbox_RAnalogV");
-		LB = Input.GetAxis ("Xbox_LBumper");
-		RB = Input.GetAxis ("Xbox_RBumper");
+		if (inputDevice == 0) {
+			RH = Input.GetAxis ("Keyboard_J+L");
+			RV = Input.GetAxis ("Keyboard_K+I");
+			//RH = Input.GetAxis ("Keyboard_J+L");
+			//RH = Input.GetAxis ("Keyboard_J+L");
+		}
+		if (inputDevice == 1) {
+			RH = Input.GetAxis ("Xbox_RAnalogH");
+			RV = Input.GetAxis ("Xbox_RAnalogV");
+			LB = Input.GetAxis ("Xbox_LBumper");
+			RB = Input.GetAxis ("Xbox_RBumper");
+		}
 
-		if (Input.GetAxis ("Horizontal") < 0.01f && Input.GetAxis ("Horizontal") > -0.01f) {
+
+
+
+		if (LH < 0.01f && LH > -0.01f) {
 			LH = 0.0f;
 		}
-		if (Input.GetAxis ("Vertical") < 0.01f && Input.GetAxis ("Vertical") > -0.01f) {
+		if (LV < 0.01f && LV > -0.01f) {
 			LV = 0.0f;
 		}
-		if (Input.GetAxis ("Xbox_RAnalogH") < 0.01f && Input.GetAxis ("Xbox_RAnalogH") > -0.01f) {
+		if (RH < 0.01f && RH > -0.01f) {
 			RH = 0.0f;
 		}
-		if (Input.GetAxis ("Xbox_RAnalogV") < 0.01f && Input.GetAxis ("Xbox_RAnalogV") > -0.01f) {
+		if (RV < 0.01f && RV > -0.01f) {
 			RV = 0.0f;
 		}
-		/*
+
 		Debug.Log ("LH: " + LH);
 		Debug.Log ("LV: " + LV);
 		Debug.Log ("RH: " + RH);
 		Debug.Log ("RV: " + RV);
-		*/
+
 		//Debug.Log (LV);
 		/*
 
