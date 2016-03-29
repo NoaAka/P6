e15dcc8bac0876664a078a93371c2328682c79a7
Peter Rossing <hprossing@gmail.com>
2016-03-29 14:55:07 +0200

    Player Control WIP

---
+++ SpaceFun/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	private InputControl input;
+	public int speed = 4;
+	public Rigidbody rb;
+
+	float moveH;
+	float moveV;
+
+	Vector3 movement;
+
+	void Start () {
+		input = GameObject.Find ("InputControl").GetComponent<InputControl> ();
+	}
+	
+	void FixedUpdate () {
+		moveH = input.LH;
+		moveV = input.LH;
+		movement = new Vector3(moveH,0.0f,moveV);
+		rb.velocity = movement * speed;
+	}
+}
