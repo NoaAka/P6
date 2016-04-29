d87c8b3fc8a9ef8c6cf35088b4e28bee7b2f8853
hanstheman83 <hey2k_bad@hushmail.com>
2016-04-29 10:29:01 +0200

    RotateForPickupAndTimeScaler
    RotateForPickupAndTimeScaler

--- SpaceFun/Assets/Pickup.cs
+++ SpaceFun/Assets/Pickup.cs
@@ -3,5 +3,10 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
     public int energy = 100;
+    public float rotation;
 
+    void Update()
+    {
+        transform.Rotate(0,rotation * Time.deltaTime, 0) ;
+    }
 }
---
+++ SpaceFun/Assets/TimeScaler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 0.2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
