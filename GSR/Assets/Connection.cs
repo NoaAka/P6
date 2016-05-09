f35d81c8902f5db57e955d9d70d90c5d5fea9ce5
hanstheman83 <hey2k_bad@hushmail.com>
2016-05-09 15:07:43 +0200

    GSR stuff
    g

--- GSR/Assets/Connection.cs
+++ GSR/Assets/Connection.cs
@@ -25,6 +25,15 @@ public class Connection : MonoBehaviour
 
     private bool hasWrittenLog = false;
 
+    private bool condition1, condition2, condition3, condition4, condition5;
+
+    private float mSec;
+    private float secThreshold = 0.070f;//depending on update time, if a frame is too slow it might miss something..
+    private float sec;
+    private float lessThanThreshold;
+    private string systemTime;
+    private float secTemp;
+
     void Start()
     {
         print("connectionPort :"+connectionPort.ToString());
@@ -35,34 +44,99 @@ public class Connection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        value = stream.ReadLine(); //Read the information
-        string[] vec2 = value.Split(','); //My arduino script returns a 3 part value (IE: 12,30,18)
-        if (vec2.Length > 1)
+
+        sec = float.Parse(System.DateTime.Now.ToString("ss"));
+        mSec = (float.Parse(System.DateTime.Now.ToString("ss.fff")) - sec); //in Seconds, but only msec component thus .000
+
+        //first solution
+        /*
+        if (mSec < secThreshold)// problem : writting two times per same timeframe due to too high secThreshold, solution : check that sec is not same as last update! 
         {
-            if (vec2[0] != "" && vec2[1] != "") //Check if all values are recieved
-            {
-                threshold = vec2[0];
-                gsrValue = vec2[1];
+            lessThanThreshold = mSec;
+          
 
-                stream.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+
+
+            value = stream.ReadLine(); //Read the information, CPU will stop untill information received
+            systemTime = System.DateTime.Now.ToString("mm:ss.fff");//when unity gets the data
+            string[] vec2 = value.Split(',');
+            if (vec2.Length > 1)
+            {
+                if (vec2[0] != "" && vec2[1] != "") //Check if all values are recieved
+                {
+                    threshold = vec2[0];
+                    gsrValue = vec2[1];
+
+                    stream.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+                }
+
+                if (logMode == LogMode.WriteToMemory)
+                {
+                    WriteToMemory();
+                }
+                else if (logMode == LogMode.WriteToFile && !hasWrittenLog)
+                {
+                    WriteToFile();
+                }
+
             }
 
-            if(logMode == LogMode.WriteToMemory)
-            {
-                WriteToMemory();
-            }
-            else if( logMode == LogMode.WriteToFile && !hasWrittenLog)
-            {
-                WriteToFile();
-            }
- 
+
         }
-        
+        print(lessThanThreshold);
+        print("system time " + systemTime);
+        */
+
+        //second solution, have to hard code it!!! since using || 
+
+
+        condition1 = mSec < secThreshold && Mathf.Abs(sec - secTemp) > 0.00001;//every 1 second
+        //update every 200 mm 
+        //if you subtract a different value from 1 and check for that.... ? first time run you subtract 0 then you subtract .2 and so on?
+        //no you want to subtract 200ms first, then 400 and so on.... make float call it subtractor, that way you get a negative value 
+        //so you make a loop and subtract more and more untill reset.... thus dynamic
+        if (condition1 ||)//update every 200ms 
+        {
+            lessThanThreshold = mSec;
+
+            secTemp = sec;
+
+
+            value = stream.ReadLine(); //Read the information, CPU will stop untill information received
+            systemTime = System.DateTime.Now.ToString("mm:ss.fff");//when unity gets the data
+            string[] vec2 = value.Split(',');
+            if (vec2.Length > 1)
+            {
+                if (vec2[0] != "" && vec2[1] != "") //Check if all values are recieved
+                {
+                    threshold = vec2[0];
+                    gsrValue = vec2[1];
+
+                    stream.BaseStream.Flush(); //Clear the serial information so we assure we get new information.
+                }
+
+                if (logMode == LogMode.WriteToMemory)
+                {
+                    WriteToMemory();
+                }
+                else if (logMode == LogMode.WriteToFile && !hasWrittenLog)
+                {
+                    WriteToFile();
+                }
+
+            }
+
+
+        }
+        print(lessThanThreshold);
+        print("system time " + systemTime);
+
+
     }
     void WriteToMemory()
     {
         gsrValues += gsrValue +"\r\n";
-        timestamps += System.DateTime.Now.ToString("hh:mm:ss.fff") +"\r\n";
+        timestamps += systemTime + "\r\n";//in minuts, seconds, and milliseconds. 
         print(System.DateTime.Now.ToString("hh:mm:ss.fff") + "threshold : " + threshold + "gsr " + gsrValue );
     }
     void WriteToFile()
--- LogProject/Assets/Log.cs
+++ LogProject/Assets/Log.cs
@@ -3,53 +3,76 @@ using System.Collections;
 
 public class Log : MonoBehaviour {
 
-    [Tooltip("update time in milliseconds")]
-    public int updateInterval;
+    [Tooltip("update time in seconds")]
+    public float updateInterval;
 
 
-    private float startDelay;
-    private string nowTime, startSecString;
+    
+    private float startTime;
     private string updateTime;
+
+    private float firstCoroutineStartTime, secondCoroutineStartTime;
+
+    private int iterator = 0;
+
+	
+    private float secThreshold = 0.070f;//depending on update time, if a frame is too slow it might miss something..
+    private float sec;
+private float lessThanThreshold;
+	
+	
+    private string systemTime;
+    
 
     // Use this for initialization
     void Start () {
-        float startSec = float.Parse(System.DateTime.Now.ToString("ss"));
-        startSecString = ("sec :" + startSec);
-        nowTime = ("Now time : "+System.DateTime.Now.ToString("ss.fff"));//TODO optimize, outcomment
         
-        startDelay = 1.0f - (float.Parse(System.DateTime.Now.ToString("ss.fff")) - startSec);
+        
+        startTime = float.Parse(System.DateTime.Now.ToString("ss.fff"));//TODO optimize, outcomment
+        
+        mSec = 1.0f - (float.Parse(System.DateTime.Now.ToString("ss.fff")) - sec); //in seconds
 
-        StartCoroutine(WaitForStartup()); //syncronize start at ms = 0, or close enough! 
+        
+        //StartCoroutine(WaitForStartup()); //syncronize start at ms = 0, or close enough! 
         
     }
 
     // Update is called once per frame
     void Update () {
         //print(System.DateTime.Now.ToString("ss.fff"));
+        //print("updateTime :"+updateTime);
+        //print("first Coroutine start time "+firstCoroutineStartTime);
+        //print("second Coroutine start time "+secondCoroutineStartTime);
 
+        //print("start system time: "+startTime);
 
+        //print("iterator : "+iterator);
+        sec = float.Parse(System.DateTime.Now.ToString("ss"));
+        mSec = (float.Parse(System.DateTime.Now.ToString("ss.fff")) - sec); //in Seconds, but only msec component thus .000
 
-        //startDelay = float.Parse(System.DateTime.Now.ToString("ss.fff"));
-        print("updateTime :"+updateTime);
+        if (mSec < secThreshold)
+        {
+            lessThanThreshold = mSec;
+            systemTime = System.DateTime.Now.ToString("mm:ss.fff");
+        }
+        print(lessThanThreshold);
+        print("system time "+systemTime);
 
-       
     }
 
     IEnumerator WaitForStartup()
     {
-        yield return new WaitForSeconds(startDelay);
+        firstCoroutineStartTime = Time.time;
+        yield return new WaitForSeconds(mSec);
         StartCoroutine(DataLogging());
     }
     IEnumerator DataLogging()
     {
-
+        secondCoroutineStartTime = Time.time;
         updateTime = System.DateTime.Now.ToString("mm:ss.fff");
-
-
+        iterator++;
         yield return new WaitForSeconds(updateInterval);
         StartCoroutine(DataLogging());
-
-
     }
 
 
